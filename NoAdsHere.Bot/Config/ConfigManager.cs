7f7fd3e8fe1bad5076d34e58c49acbb44764affc
Nanabell <marlon.delfin@gmail.com>
2018-04-15 12:14:36 +0200

    Initial Base

---
+++ NoAdsHere.Bot/Config/ConfigManager.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace NoAdsHere.Bot.Config
+{
+    public class ConfigManager
+    {
+        private const string FilePath = "configuration.json";
+
+        public GlobalConfig GlobalConfig { get; set; } = new GlobalConfig();
+
+        /// <summary>
+        /// Load The Configuration from File and update the current instance with it
+        /// </summary>
+        /// <returns>Also returns the current instance to use it fluently</returns>
+        public ConfigManager Load()
+        {
+            if (!File.Exists(FilePath))
+                Save();
+
+            var cm = JsonConvert.DeserializeObject<ConfigManager>(File.ReadAllText(FilePath));
+            GlobalConfig = cm.GlobalConfig;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Save the current configuration to File
+        /// </summary>
+        public void Save() => File.WriteAllText(FilePath, JsonConvert.SerializeObject(this, Formatting.Indented));
+    }
+}
---
+++ NoAdsHere.Bot/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.MicrosoftLogging;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NoAdsHere.Bot.Config;
+
+namespace NoAdsHere.Bot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+            => new Program().MainAsync(args).GetAwaiter().GetResult();
+
+        private IServiceProvider _provider;
+
+        private ILogger _logger;
+        private DiscordShardedClient _client;
+
+        // ReSharper disable once UnusedParameter.Local
+        private async Task MainAsync(string[] args)
+        {
+            _provider = BuildServiceProvider();
+
+            _logger = _provider.GetRequiredService<ILogger<Program>>();
+            _logger.LogInformation("Hello World!");
+
+            var cm = _provider.GetRequiredService<ConfigManager>();
+            cm.Load();
+
+            _client = _provider.GetRequiredService<DiscordShardedClient>();
+            _client.UseMicrosoftLogging(_provider.GetRequiredService<ILogger<DiscordShardedClient>>());
+
+            await _client.LoginAsync(TokenType.Bot, cm.GlobalConfig.Token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        /// <summary>
+        /// Create a new ServiceProvider / Dependency Injection service
+        /// </summary>
+        /// <returns>A Service Provider with logging already Setup</returns>
+        private static IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
+            services.AddSingleton<ConfigManager>();
+            services.AddSingleton(new DiscordShardedClient(
+                new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Debug,
+                    TotalShards = GetRecommndedShardCount()
+                }));
+            services.AddSingleton(new CommandService(
+                new CommandServiceConfig
+                {
+                    LogLevel = LogSeverity.Debug,
+                    IgnoreExtraArgs = true
+                }));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Set up Logging
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            // Set up Logger Targets
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+            return serviceProvider;
+        }
+
+        private static int GetRecommndedShardCount()
+        {
+            var client = new DiscordSocketClient();
+            var cm = new ConfigManager().Load();
+
+            client.LoginAsync(TokenType.Bot, cm.GlobalConfig.Token).GetAwaiter().GetResult();
+            
+            return client.GetRecommendedShardCountAsync().GetAwaiter().GetResult();
+        }
+    }
+}
